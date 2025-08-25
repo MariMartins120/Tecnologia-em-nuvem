77c7bd6a045ba53ba276b11644fc9c6f231a8dd4
MariMartins120 <163855488+MariMartins120@users.noreply.github.com>
2025-08-25 10:47:23 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8"
+</head>
+<body>
+  <h1>Este é um titulo principal</h1>
+  <p>Este é um paragrafo.</p>
+  <a href="https://www.exemplo.com">Este é um link</a>
+  
+</body>
+</html>
