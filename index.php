67a93152cb9b9e300bfa0454db2f872da04c95e9
MariMartins120 <163855488+MariMartins120@users.noreply.github.com>
2025-09-15 09:19:01 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,38 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-  <meta charset="UTF-8"
+    <meta charset="UTF-8">
+    <title>Formulário de contato</title>
 </head>
 <body>
-  <h1>Este é um titulo principal</h1>
-  <p>Este é um paragrafo.</p>
-  <a href="https://www.exemplo.com">Este é um link</a>
+    <h1>Fale conosco</h1>
+  <form  method="post" action="">
+    
+  <label for="nome">Nome:</label><br>
+  <input type="text" name="nome" id="nome" required ><br><br>
+    
+  <label for="email">Email:</label><br>
+  <input type="text" name="email" id="email" required ><br><br>
+    
+  <label for="mensagem">Mensagem:</label><br>
+  <textarea name="mensagem" id="mensagem" rows="5" required></textarea><br><br>
+    
+  <input type="submit" value="Enviar">
+  </form>
+<?php
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+          // Coleta os dados do formulario
+            $nome = htmlspecialchars($_POST['nome']);
+            $email = htmlspecialchars($_POST['email']);
+            $mensagem = htmlspecialchars($_POST['mensagem']);
   
+ //Exibe os dados (em uma aplicação real, você poderia salvar ou enviar por email)
+    
+    echo "<h2>Dados Recebidos:</h2>";
+    echo "<p><strong>Nome:</strong>$nome</p>"; 
+    echo "<p><strong>Email:</strong>$email</p>"; 
+    echo "<p><strong>Mensagem:</strong>$mensagem</p>"; 
+  }
+  ?>
 </body>
 </html>
